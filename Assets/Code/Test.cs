1de5cfd7f6877ae32ab9eae4ad8fa496c785d3c2
Kevin43 <prekasstamos@gmail.com>
2018-08-07 21:37:21 -0400

    Added ability to load map in-game
    This is a step in the ability to pick maps to load in-game.

--- Assets/Code/Test.cs
+++ Assets/Code/Test.cs
@@ -1,67 +1,86 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using uSrcTools;
+using UnityEditor;
 using UnityEngine;
+
+[CustomEditor (typeof (Test))]
+public class TestEditor : Editor
+{
+	//string mapName;
+	
+	public override void OnInspectorGUI ()
+	{
+		DrawDefaultInspector ();
+		//mapName = EditorGUILayout.TextField ("Map Name", "");
+		if (GUILayout.Button ("Load Map"))
+		{
+			Test.Inst.bsp.Load (Test.Inst.mapName);
+		}
+	}
+}
 
 namespace uSrcTools
 {
 	public class Test : MonoBehaviour
+	{
+		public static Test Inst;
+
+		public SourceBSPLoader bsp;
+		public SourceStudioModel model;
+		public Material testMaterial;
+		public Texture cameraTexture;
+
+		public string exportLocation = "D:\\uSource\\Example\\";
+		public string mapName;
+		public string modelName;
+		public bool skinnedModel = false;
+		public Transform player;
+		public Transform playerCamera;
+		public Transform skyCamera;
+		public Light light_environment;
+		public Vector3 skyCameraOrigin;
+		public Vector3 startPos;
+
+		public bool loadMap = true;
+		public bool loadModel = false;
+		public bool exportMap = false;
+		public bool isL4D2 = false;
+		public bool forceHDR = false;
+		public bool skipSky = true;
+
+		void Awake ()
 		{
-			public static Test Inst;
+			Inst = this;
+		}
 
-			public SourceBSPLoader bsp;
-			public SourceStudioModel model;
-			public Material testMaterial;
-			public Texture cameraTexture;
+		void Start ()
+		{
+			//player.transform.position = GameObject.Find ("info_player_start").transform.position;
 
-			public string exportLocation = "D:\\uSource\\Example\\";
-			public string mapName;
-			public string modelName;
-			public bool skinnedModel = false;
-			public Transform player;
-			public Transform playerCamera;
-			public Transform skyCamera;
-			public Light light_environment;
-			public Vector3 skyCameraOrigin;
-			public Vector3 startPos;
-
-			public bool loadMap = true;
-			public bool loadModel = false;
-			public bool exportMap = false;
-			public bool isL4D2 = false;
-			public bool forceHDR = false;
-			public bool skipSky = true;
-
-			void Awake ()
+			if (loadMap)
 			{
-				Inst = this;
-			}
+				if (bsp == null)
+					bsp = GetComponent<SourceBSPLoader> ();
 
-			void Start ()
+				// When game starts, it goes to SourceBSPLoader and the Load function.
+				bsp.Load (mapName);
+				if (exportMap)
 				{
-					//player.transform.position = GameObject.Find ("info_player_start").transform.position;
-
-					if (loadMap)
-					{
-						if (bsp == null)
-							bsp = GetComponent<SourceBSPLoader> ();
-
-						bsp.Load (mapName);
-						if (exportMap)
-						{
-							COLLADAExport.Geometry g = bsp.map.BSPToGeometry ();
-							print ("Exporting map.");
-							//COLLADAExport.Export(@"I:\uSource\test\"+mapName+".dae",g,false,false);
-							COLLADAExport.Export (exportLocation+mapName+".dae ",g,false,false);
+					COLLADAExport.Geometry g = bsp.map.BSPToGeometry ();
+					print ("Exporting map.");
+					//COLLADAExport.Export(@"I:\uSource\test\"+mapName+".dae",g,false,false);
+					COLLADAExport.Export (exportLocation + mapName + ".dae ", g, false, false);
 				}
 			}
 
-			if(loadModel)
+			if (loadModel)
 			{
-				GameObject modelObj=new GameObject("TestModel ");
-				model.Load (@"models / "+modelName+".mdl ");
+				GameObject modelObj = new GameObject ("TestModel ");
+				model.Load (@"models / " + modelName + ".mdl ");
 				//model.GetInstance(modelObj,skinnedModel);
-				model.GetInstance (modelObj,skinnedModel,0);
+				model.GetInstance (modelObj, skinnedModel, 0);
 				//modelObj.transform.localEulerAngles=new Vector3(270,0,0);
 			}
 		}
@@ -73,7 +92,7 @@ namespace uSrcTools
 
 		void OnDrawGizmos ()
 		{
-			if (model!=null)
+			if (model != null)
 				model.OnDrawGizmos ();
 		}
 	}
